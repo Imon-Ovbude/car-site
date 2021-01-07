64c4c7e70f8a041ea96f893b70bb255944c1cfb4
Imon-Ovbude <imonhimio@gmail.com>
2021-01-06 19:56:52 -0800

    nineteenth commit, add function to set current time in datetime form input

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,25 @@
 const dateTime = document.getElementById('date-time');
-const today = new Date();
+let today = new Date();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+let year = today.getFullYear();
+let hours = today.getHours();
+let mins = today.getMinutes();
+
+// Set Minimum Date And Time Attribute and Value
+function setDateAttr() {
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  today = `${year}-${month}-${day}T${hours}:${mins}`;
+
+  dateTime.setAttribute('min', today);
+  dateTime.setAttribute('value', today);
+}
+
+setDateAttr();
